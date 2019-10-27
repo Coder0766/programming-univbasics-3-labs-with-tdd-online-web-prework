e6c97a1e0f293ce84347e6ed3437cfe7981ae637
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-27 04:17:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,11 @@
+#Lab with Test Driven Development
 # Code your solution in calculator.rb
 
-#Test I
-first_number = 10
 
-#Test II
-second_number =< 0
+puts first_number  =  10              #Test I
+puts second_number =  1000            #Test II
+
+sum= first_number + second_number #Test III
+difference  = first_number - second_number #Test VI
+product  = first_number * second_number #Test V
+quotient  = first_number / second_number #Test VI
